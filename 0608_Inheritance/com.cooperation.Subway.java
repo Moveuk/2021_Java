0dea16bb839c96ee820d60ca7dbeaf22893d5854
Moveuk <84966961+Moveuk@users.noreply.github.com>
2021-06-08 12:00:11 +0900

    Create com.cooperation.Subway.java
    // Key Word : 포함 관계, 상속(inheritance)

---
+++ 0608_Inheritance/com.cooperation.Subway.java
@@ -0,0 +1,31 @@
+// 이것이 자바다 _ 신용권 : 288p
+// Key Word : 포함 관계, 상속(inheritance)
+
+// 포함 관계 : student가 bus에 있는 인자를 가져와서 사용.
+// 상속 : A의 내용이 B에도 필요한 내용이기 때문에 A의 값을 상속받아 사용 가능하다.
+// 상속 대상 : 변수와 메소드만 상속이 가능하다 
+//			 private, 생성자는 상속이 불가능.
+//----------------------------------------------------------------------
+
+package com.cooperation;
+
+public class Subway {
+	String lineNumber;
+	int passengerCount;
+	int money;
+	
+	public Subway(String lineNumber) {
+		super();
+		this.lineNumber = lineNumber;
+	}
+	
+	void take(int money) {
+		passengerCount++;
+		this.money += money;
+	}
+	
+	void showInfo() {
+		System.out.println("지하철 " + lineNumber + "호선의 승객 수는 " + passengerCount);
+		System.out.println("수익은 " + money + "원 입니다.");
+	}
+}
