838843037d0d90de31f45985133747ca491611f2
Moveuk <84966961+Moveuk@users.noreply.github.com>
2021-06-08 17:10:29 +0900

    Create protected_modi.package1.Apack.java
    // Key Word : protected, 접근제어자(Access Modifier)

---
+++ 0608_Inheritance/protected_modi.package1.Apack.java
@@ -0,0 +1,13 @@
+// 이것이 자바다 _ 신용권 : 303p
+// Key Word : protected, 접근제어자(Access Modifier)
+// protected : 내부 패키지 혹은 자식 클래스 내부에서만 사용 가능하다는 뜻의 접근 제어자.
+//----------------------------------------------------------------------
+
+package protected_modi.package1;
+
+public class Apack {
+	private int x;
+	protected int y;
+	public int z;
+
+}
