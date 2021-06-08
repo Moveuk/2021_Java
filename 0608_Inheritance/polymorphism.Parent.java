68b3b79c3f7db0fcd5e0a8e4d1876b75406a29af
Moveuk <84966961+Moveuk@users.noreply.github.com>
2021-06-08 17:12:19 +0900

    Create polymorphism.Parent.java
    // Key Word : 다형성(Polymorphism), 자동 타입 변환 (promotion)

---
+++ 0608_Inheritance/polymorphism.Parent.java
@@ -0,0 +1,17 @@
+// 이것이 자바다 _ 신용권 : 305p
+// Key Word : 다형성(Polymorphism), 자동 타입 변환 (promotion)
+// 다형성(Polymorphism) : 같은 타입이지만 실행 결과가 다양한 객체를 이용할 수 있는 성질.
+// 
+//----------------------------------------------------------------------
+
+package polymorphism;
+
+public class Parent {
+	public void method1() {
+		System.out.println("Parent-methos1()");
+	}
+	
+	public void method2() {
+		System.out.println("Parent-methos2()");
+	}
+}
